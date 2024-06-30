fe300404d9cd5a39b861f8e96357077a51f871d4
Sunny <p147896325p@gmail.com>
2024-06-30 19:53:01 +0800

    Use plotly to plot  annual income third quantile trend.

--- 收入分析.R
+++ 收入分析.R
@@ -1,18 +1,59 @@
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 # 讀取資料
 all_income_df = read.csv('data/result/income_data.csv',stringsAsFactors = F)
 
+
+
 # 塞選縣市
 sub_income_df = 
   all_income_df %>%
-  filter(city  %in% c('新竹市'))
+  filter(city  %in% c('新竹市'))%>%
+  mutate(year = as.character(year))
+
+# 留下
+# 前1/5收入的裡
+n_li = as.integer(length(unique(sub_income_df$village))/5)
+# 全部的里
+# n_li = as.integer(length(unique(sub_income_df$village)))
+
+
+# 用最新一年的收入的三分位來比較
+max_year = max(sub_income_df$year)
+keep_li = sub_income_df %>%
+  filter(year == max_year) %>%
+  slice_max(三分位,n=n_li) %>%
+  distinct(village)
+keep_li = keep_li$village
+sub_income_df = 
+  sub_income_df %>%
+  filter(village %in% keep_li ) 
+
 
 # 畫圖
-ggplot(sub_income_df, aes(x = year , y = 三分位 , color = village )) +
-  geom_line()
+#ggplot(sub_income_df, aes(x = year , y = 三分位 , color = village,group = village )) +
+  #geom_line()
+plot_ly(sub_income_df, x = ~year, y = ~三分位, color = ~village, type = 'scatter', mode = 'lines') %>%
+  layout(
+    title = "Year and 收入第三分位數 趨勢圖",
+    xaxis = list(title = "Year"),  # 设置X轴名称
+    yaxis = list(title = "收入第三分位數(千)")          # 设置Y轴名称
+  )
 
-# city is_presale trading_year_quater_plot house_type house_year_stratidied
+# 
+total_income_df = 
+  sub_income_df%>%
+  group_by(year )%>%
+  summarise(三分位 = mean(三分位)) %>%
+  ungroup()
+
+plot_ly(total_income_df, x = ~year, y = ~三分位, type = 'scatter', mode = 'lines') %>%
+  layout(
+    title = "Year and 收入第三分位數(全里平均) 趨勢圖",
+    xaxis = list(title = "Year"),  # 设置X轴名称
+    yaxis = list(title = "收入第三分位數(千)")          # 设置Y轴名称
+  )
 
 
